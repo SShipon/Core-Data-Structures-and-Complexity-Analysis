f2141caa1850679cbc6d1f9db4bfe3f455767018
SShipon <nxshipon@gmail.com>
2025-04-24 11:53:25 -0700

    number 2 problem find sum of array done

--- part-1/part1.js
+++ part-1/part1.js
@@ -1,4 +1,4 @@
-// find maximum element in array 
+//  number 1 problem find maximum element in array 
 function findMax(arr) {
     let max = arr[0];
     for (let i = 1; i < arr.length; i++) {
@@ -11,3 +11,18 @@ function findMax(arr) {
  //console.log( findMax([1, 3, 5, 2, 9]))
  // time complexity : o(n)
  // space complexity : o(1)
+
+
+// number 1 problem Find sum of array 
+
+function findSum(arr){
+    let sum = 0;
+    for(let num of arr){
+        sum += num;
+    }
+    return sum;
+}
+
+//console.log( findSum([1, 3, 5, 2, 9]))
+// time complextiy: o(n)
+// space complexity : o(1)
