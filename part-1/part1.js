e35cab4aa7650c8427366273ebdd5fcfa5d4e727
SShipon <nxshipon@gmail.com>
2025-04-24 15:28:40 -0700

    problem number 3 binary seach work done

--- part-1/part1.js
+++ part-1/part1.js
@@ -9,11 +9,9 @@ function findMax(arr) {
     return max;
   }
  //console.log( findMax([1, 3, 5, 2, 9]))
- // time complexity : o(n)
- // space complexity : o(1)
 
 
-// number 1 problem Find sum of array 
+// number 2 problem Find sum of array 
 
 function findSum(arr){
     let sum = 0;
@@ -24,5 +22,18 @@ function findSum(arr){
 }
 
 //console.log( findSum([1, 3, 5, 2, 9]))
-// time complextiy: o(n)
-// space complexity : o(1)
+
+
+// number 3 problem Binary Search 
+function binarySearch(arr, target) {
+    let left = 0, right = arr.length - 1;
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (arr[mid] === target) return mid;
+      else if (arr[mid] < target) left = mid + 1;
+      else right = mid - 1;
+    }
+    return -1;
+  }
+
+  //console.log( binarySearch([1, 3, 5, 2, 9]))
